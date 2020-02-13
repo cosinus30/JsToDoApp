481555deb9fc8a66565543bc57c55e045f3a7580
Yunus Kerem Turk <krmtrk20@gmail.com>
2020-02-13 21:16:41 +0300

    Remove item functionality is added.

--- App/assets/js/todos.js
+++ App/assets/js/todos.js
@@ -2,3 +2,13 @@
 $("li").click(function() {
   $(this).toggleClass("completed");
 });
+
+//Click on X to delete todo
+$("span").click(function(event) {
+  event.stopPropagation(); //stop the event bubbling.
+  $(this)
+    .parent()
+    .fadeOut(500, function() {
+      $(this).remove();
+    });
+});
