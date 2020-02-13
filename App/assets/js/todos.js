bfabc6b0af191d47f73de9ed511cf9c9179b46e3
Yunus Kerem Turk <krmtrk20@gmail.com>
2020-02-13 21:00:06 +0300

    Novice version of make it done effect is added.
    All data is dummy, and not dynamic. Step by step dude.

---
+++ App/assets/js/todos.js
@@ -0,0 +1,18 @@
+// Check of specific todos by clicking
+$("li").click(function() {
+  //if li is gray
+  //turn it black
+  //else
+  //turn it gray
+  if ($(this).css("color") === "rgb(128, 128, 128)") {
+    $(this).css({
+      color: "black",
+      textDecoration: "none"
+    });
+  } else {
+    $(this).css({
+      color: "gray",
+      textDecoration: "line-through"
+    });
+  }
+});
