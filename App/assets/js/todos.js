0a64a1a8c5fd088507cd892f69062a2641d16251
Yunus Kerem Turk <krmtrk20@gmail.com>
2020-02-13 21:31:08 +0300

    Add a todo functionality is added. click events are changed.
    jQuery's .click() event does not apply it's rule to dynamically created elements. To make it happen I changed the event to on("click"), and then I listen the <ul> which will always be on the page.

--- App/assets/js/todos.js
+++ App/assets/js/todos.js
@@ -1,14 +1,23 @@
 // Check of specific todos by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
 });
 
 //Click on X to delete todo
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
   event.stopPropagation(); //stop the event bubbling.
   $(this)
     .parent()
     .fadeOut(500, function() {
       $(this).remove();
     });
+});
+
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    var todoText = $(this).val();
+    $(this).val("");
+    //create a new li and add to ul
+    $("ul").append("<li><span> X </span>" + todoText + "</li>");
+  }
 });
